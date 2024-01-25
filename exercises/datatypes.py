d3b9faa27c4ec1a536ea2179a8b384a8e47a5ac7
Joris Hoendervangers <joris@pythonsherpa.com>
2024-01-25 13:38:58 +0100

    first exercise

---
+++ exercises/datatypes.py
@@ -0,0 +1,14 @@
+a = 1995
+b = 1.0
+c = "234"
+d = "\u20AC"
+e = False
+f = 100 * 10
+g = "True"
+h = len("hello world")
+i = 100 ** 10
+j = 5 <= 7
+k = 4 / 3
+l = [a, b, c]  # noqa E741
+
+print(type(a))
