d24c14eebda9694a5af14645742c12efbe27b191
Joris Hoendervangers <joris@pythonsherpa.com>
2024-01-30 13:56:11 +0100

    flow

---
+++ demos/demo_6_flow.py
@@ -0,0 +1,23 @@
+"""
+Control flow / branching
+"""
+
+a = 4
+b = 5
+print(a < b)
+
+my_list = []
+
+if my_list:
+    print(my_list)
+    print("end of program")
+
+
+temperature = 19
+
+if temperature > 20:
+    print("it is hot")
+elif temperature > 18:
+    print("it is nice")
+else:
+    print("it is cold")
