cb4f1fa03df3c0f7cd7a1a74aa6d23065eb19c4b
Joris Hoendervangers <joris@pythonsherpa.com>
2024-02-01 16:29:42 +0100

    final

--- exercises/logging_exercise.py
+++ exercises/logging_exercise.py
@@ -1,25 +1,35 @@
-from loguru import logger
+import logging
 import secrets
 
-logger.warning("Program is for demo purposes only")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    # filename="logging_exercise.log",
+    # filemode="w"
+)
 
 
 def main():
     """Run program to email a secret token"""
+    logging.info("Start program")
     token = generate_token()
     email_token(token)
+    logging.info("End program")
 
 
 def generate_token():
     """Return a token for password reset"""
+    logging.info("Generating token")
     token = secrets.token_hex()
+    logging.debug(f"Token: {token}")
     return token
 
 
 def email_token(token):
     """Email the token"""
+    logging.info("Emailing the token")
     body = f"The token to reset your password: {token}"
-    logger.debug(body)
+    logging.debug(body)
 
 
 if __name__ == "__main__":
